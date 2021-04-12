4964b33ffb386f8a1b8cbb99e3399c8b0db5cec3
simonhlee97 <simonhlee97@hotmail.com>
2021-04-12 19:35:45 +0900

    added env

--- simonpostcom/config.py
+++ simonpostcom/config.py
@@ -2,8 +2,7 @@ import os
 
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = '72try8u6r54e4s3wdy(*(!mn982323u'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///simonpostcom.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
